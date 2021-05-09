83ab6357756636c1ae00e2b44dd52ce7960040d3
unknown <admin@PNVL47.pn.com.vn>
2021-05-09 21:51:35 +0700

    process image

--- main.js
+++ main.js
@@ -2,13 +2,48 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
+const atob= require("atob");
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
-
+server.use(jsonServer.bodyParser)
 // Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
+server.get('/api/echo', (req, res) => {
+  return res.json({a: 1})
+})
+
+const host= __dirname
+// function dataURLtoFile(dataurl, filename) {
+ 
+//   var arr = dataurl.split(','),
+//       mime = arr[0].match(/:(.*?);/)[1],
+//       bstr = atob(arr[1]), 
+//       n = bstr.length, 
+//       u8arr = new Uint8Array(n);
+      
+//   while(n--){
+//       u8arr[n] = bstr.charCodeAt(n);
+//   }
+  
+//   return new File([u8arr], filename, {type:mime});
+// }
+
+//Usage example:
+// var file = dataURLtoFile('data:text/plain;base64,aGVsbG8gd29ybGQ=','hello.txt');
+// console.log(file);
+
+server.put('/api/ads',(req,res)=>{
+  const data=req.body['img']
+  const fileType=data.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0].split('/')[1]
+  var endFile=""
+  if( fileType=="jpeg"){
+    endFile="jpg"
+  }
+  const date=new Date()
+  const filename=date.getTime()+"."+endFile
+  require("fs").writeFile(filename, data.split(',')[1], 'base64', function(err) {
+    console.log(err);
+  });
+  res.jsonp({img: `${host} \ ${filename}`})
 })
 
 // To handle POST, PUT and PATCH you need to use a body-parser
