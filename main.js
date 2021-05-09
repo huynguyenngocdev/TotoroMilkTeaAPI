14aeecd98a044b309f526f3d73d5b9ca40fbb514
unknown <admin@PNVL47.pn.com.vn>
2021-05-10 03:22:47 +0700

    process upload and delete old image on server

--- main.js
+++ main.js
@@ -1,62 +1,71 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-const atob= require("atob");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const atob = require("atob");
+const { send } = require("process");
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
 // Add custom routes before JSON Server router
-server.get('/api/echo', (req, res) => {
-  return res.json({a: 1})
-})
 
-const host= __dirname
-// function dataURLtoFile(dataurl, filename) {
- 
-//   var arr = dataurl.split(','),
-//       mime = arr[0].match(/:(.*?);/)[1],
-//       bstr = atob(arr[1]), 
-//       n = bstr.length, 
-//       u8arr = new Uint8Array(n);
-      
-//   while(n--){
-//       u8arr[n] = bstr.charCodeAt(n);
-//   }
-  
-//   return new File([u8arr], filename, {type:mime});
-// }
+const host = __dirname;
+// 'http://totoromilkteaapi.herokuapp.com/';
 
-//Usage example:
-// var file = dataURLtoFile('data:text/plain;base64,aGVsbG8gd29ybGQ=','hello.txt');
-// console.log(file);
+server.use(jsonServer.bodyParser);
 
-server.put('/api/ads',(req,res)=>{
-  const data=req.body['img']
-  const fileType=data.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0].split('/')[1]
-  var endFile=""
-  if( fileType=="jpeg"){
-    endFile="jpg"
+server.use((req, res, next) => {
+  if (req.method === "PUT" && req.path === "/api/ads") {
+    // require("fs").unlink(
+
+    // );
+    const date = new Date();
+    let updateAt = date.getTime();
+
+    const data = req.body;
+    const image = data["image"];
+    const fileType = image.match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0].split("/")[1];
+    var endFile = "";
+    if (fileType == "jpeg") {
+      endFile = "jpg";
+    } else if (fileType == "png") {
+      endFile = "png";
+    }
+
+    const filename = date.getTime() + "." + endFile;
+    // create new image ads
+    require("fs").writeFile(
+      `image_ads/${filename}`,
+      image.split(",")[1],
+      "base64",
+      function (err) {
+        console.log(err);
+      }
+    );
+    // delete old image ads
+    let files = require("fs").readdirSync(`./image_ads`);
+
+    let file = files.filter((image)=>{
+      return image.indexOf(`${data.updateAt}`) >= 0;
+    });
+
+    require("fs").unlink(`./image_ads/${file}`, function (err) {
+      if (err) throw err;
+    });
+
+    data.updateAt = updateAt
+    // req.body["oldImage"] = req.body["oldImage"] =  
+
+    data.image = `${host}/image_ads/${filename}`;
+
+    req.body = data;
+    res.jsonp(req.body);
   }
-  const date=new Date()
-  const filename=date.getTime()+"."+endFile
-  require("fs").writeFile(filename, data.split(',')[1], 'base64', function(err) {
-    console.log(err);
-  });
-  res.jsonp({img: `${host} \ ${filename}`})
-})
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-// server.use((req, res, next) => {
-//   if (req.method === 'POST') {
-//     req.body.createdAt = Date.now(),
-//     req.body.updatedAt = Date.now()
-//   }
-//   // Continue to JSON Server router
-//   next()
-// })
+  // Continue to JSON Server router
+  next();
+});
 
 // router.render = (req, res) => {
 //     const header = res.getHeaders()
@@ -77,12 +86,11 @@ server.use(jsonServer.bodyParser)
 //     })
 //   }
 
-
 // set port
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 // Use default router
-server.use('/api',router)
+server.use("/api", router);
 // Start server
 server.listen(PORT, () => {
-  console.log("TotoroMilkTea website's JSON Server is running")
-})
+  console.log("TotoroMilkTea website's JSON Server is running");
+});
