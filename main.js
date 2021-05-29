f7ed80203e390aea93657a20acacbebad2ae7f29
huynguyen321 <huy.nguyen22@student.passerellesnumeriques.org>
2021-05-29 18:12:31 +0700

    update server

--- main.js
+++ main.js
@@ -85,12 +85,14 @@ server.get("/api/get_image/*", (req, res) => {
 
 // put ads
 server.use((req, res, next) => {
-  if (req.method === "PUT" && req.path.indexOf("/api/products") >= 0) {
+  if (req.method === "PUT" && req.path.indexOf("/api/ads") >= 0) {
     const date = new Date();
     let updateAt = date.getTime();
 
     const data = req.body;
+
     const image = data["image"];
+
     // process to a image file if it is a new image
     if (image.indexOf(".png") < 0 && image.indexOf(".jpg") < 0) {
       const fileType = image
@@ -181,12 +183,14 @@ server.use((req, res, next) => {
 
 // put product
 server.use((req, res, next) => {
-  if (req.method === "PUT" && req.path === "/api/ads") {
+  if (req.method === "PUT" && req.path.indexOf("/api/products/") >= 0) {
     const date = new Date();
     let updateAt = date.getTime();
 
     const data = req.body;
+
     const image = data["image"];
+
     // process to a image file if it is a new image
     if (image.indexOf(".png") < 0 && image.indexOf(".jpg") < 0) {
       const fileType = image
@@ -197,71 +201,22 @@ server.use((req, res, next) => {
         endFile = "jpg";
       } else if (fileType == "png") {
         endFile = "png";
+      } else {
+        res.send("Error!");
       }
 
       const filename = date.getTime() + "." + endFile;
-      // create new image ads
+      // create new image products
       fs.writeFile(
-        `image_ads/${filename}`,
+        `image_products/${filename}`,
         image.split(",")[1],
         "base64",
         function (err) {
           console.log(err);
         }
       );
-      // delete old image ads
-      let files = fs.readdirSync(`./image_ads`);
 
-      let file = files.filter((image) => {
-        return image.indexOf(`${data.updateAt}`) >= 0;
-      });
-
-      fs.unlink(`./image_ads/${file}`, function (err) {
-        if (err) throw err;
-      });
-
-      data.updateAt = updateAt;
-      // req.body["oldImage"] = req.body["oldImage"] =
-
-      data.image = `image_ads/${filename}`;
-      req.body = data;
-    }
-
-    res.jsonp(req.body);
-  }
-
-  // Continue to JSON Server router
-  next();
-});
-
-// delete image product
-server.use((req, res, next) => {
-  if (req.path.indexOf("/api/delete_image/") >= 0) {
-    const data = req.body;
-    const image = data["image"];
-    // process to a image file if it is a new image
-    if (image.indexOf(".png") < 0 && image.indexOf(".jpg") < 0) {
-      const fileType = image
-        .match(/[^:]\w+\/[\w-+\d.]+(?=;|,)/)[0]
-        .split("/")[1];
-      var endFile = "";
-      if (fileType == "jpeg") {
-        endFile = "jpg";
-      } else if (fileType == "png") {
-        endFile = "png";
-      }
-
-      const filename = date.getTime() + "." + endFile;
-      // create new image ads
-      fs.writeFile(
-        `image_ads/${filename}`,
-        image.split(",")[1],
-        "base64",
-        function (err) {
-          console.log(err);
-        }
-      );
-      // delete old image ads
+      // delete old image products
       let files = fs.readdirSync(`./image_products`);
 
       let file = files.filter((image) => {
@@ -271,11 +226,32 @@ server.use((req, res, next) => {
       fs.unlink(`./image_products/${file}`, function (err) {
         if (err) throw err;
       });
-    }
-  }
 
-  // Continue to JSON Server router
+      data.updateAt = updateAt;
+
+      data.image = `image_products/${filename}`;
+      req.body = data;
+    }
+    res.jsonp(req.body);
+  }
   next();
+});
+
+// delete image product
+server.post("/api/delete_image/*", (req, res) => {
+  // get folder
+  const folder = req.path.split("/")[3];
+  //get all name of image of image_products
+  const files = fs.readdirSync(`./${folder}`);
+  // get file name
+  const filename = req.path.split("/")[4];
+
+  if (files.findIndex((file) => file == filename) >= 0) {
+    fs.unlink(`./${folder}/${filename}`, function (err) {
+      if (err) throw err;
+    });
+    res.send("Delete image success");
+  } else res.send("Not Found Image In DB");
 });
 
 // set port
